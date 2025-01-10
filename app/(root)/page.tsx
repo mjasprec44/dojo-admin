34103e652d8a3e03c15d49b8141f40ca0ffb4071
MJAsprec <michael.asprec@whitecloak.com>
2025-01-10 14:38:35 +0800

    shadcn dialog box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,16 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+// import { UserButton } from "@clerk/nextjs";
+import { Modal } from "@/components/ui/modal";
 export default function SetupPage() {
   return (
     <div className="p-4">
-      <UserButton />
+      {/* <UserButton /> */}
+      <Modal
+        isOpen={true}
+        onClose={() => {}}
+        title="test"
+        description="test desc"
+      ></Modal>
     </div>
   );
 }
