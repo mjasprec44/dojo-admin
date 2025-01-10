3bdf278112e4c9644eb8cd896f6deed1942ad652
MJAsprec <michael.asprec@whitecloak.com>
2025-01-10 16:22:51 +0800

    modalProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
 import { ClerkProvider } from "@clerk/nextjs";
+
+import { ModalProvider } from "@/providers/modal-provider";
 
 import "./globals.css";
 
@@ -31,6 +32,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <ModalProvider />
           {children}
         </body>
       </html>
