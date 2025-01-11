58207905173b9823a2f36df6bc11cca6a9c91cb0
MJAsprec <michael.asprec@whitecloak.com>
2025-01-11 19:51:02 +0800

    toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from "@/providers/toast-provider";
 
 import "./globals.css";
 
@@ -32,7 +33,9 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <ToasterProvider />
           <ModalProvider />
+
           {children}
         </body>
       </html>
